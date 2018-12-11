fe86312ab714cc844e399819e3a8bd96899f7627
Loic PASCAL <loic.pascal@gmail.com>
2018-12-12 00:34:27 +0100

    first commit

---
+++ iaLoic.js
@@ -0,0 +1,197 @@
+var IA = {
+    log: false,
+
+    /**
+     * Joue pour l'IA
+     * @param unePartie
+     * @param profondeur
+     */
+    jouer: function (unePartie, profondeur) {
+        var tmp;
+        var maxCol = 1;
+        var max = -10000;
+        let row;
+
+        for (var col = 1; col <= game.nbColumn; col++) {
+            game.joueurActif = 'player2';
+            row = game.placerPiece(col, "player2");
+            tmp = this.min(unePartie, profondeur-1);
+            if (tmp > max) {
+                max = tmp;
+                maxCol = col;
+            }
+            game.retirerPiece(col, row);
+        }
+        game.placerPiece(maxCol, "player2", true);
+    },
+
+    /**
+     *
+     * @param partie
+     * @param profondeur
+     * @returns {*}
+     */
+    max: function (partie, profondeur) {
+        if (profondeur === 0 || this.gagnant(partie) !== "aucun") {
+            return this.evaluer(partie);
+        }
+
+        var max = -10000;
+        let tmp, row;
+
+        for (var col = 1; col <= game.nbColumn; col++) {
+            game.joueurActif = 'player2';
+            row = game.placerPiece(col, "player2");
+            tmp = this.min(partie, profondeur-1);
+
+            if (tmp > max) {
+                max = tmp;
+            }
+            game.retirerPiece(col, row);
+        }
+        return max;
+    },
+
+    /**
+     *
+     * @param unePartie
+     * @param profondeur
+     * @returns {*}
+     */
+    min: function (unePartie, profondeur) {
+        if (profondeur === 0 || this.gagnant(unePartie) !== "aucun") {
+            return this.evaluer(unePartie);
+        }
+
+        var min = 10000;
+        var tmp, row;
+
+        for (var col = 1; col <= game.nbColumn; col++) {
+            game.joueurActif = 'player1';
+            row = game.placerPiece(col, "player1");
+            tmp = this.max(unePartie, profondeur-1);
+
+            if (tmp < min) {
+                min = tmp;
+            }
+            game.retirerPiece(col, row);
+        }
+        return min;
+    },
+
+    /**
+     * Evalue le poids d'un état de la partie
+     * @param unePartie
+     * @returns {*}²
+     */
+    evaluer: function(unePartie) {
+        let vainqueur = this.gagnant(unePartie);
+        // Si on a un gagnant
+        if (vainqueur !== "aucun") {
+            if (vainqueur === "player2") {
+                return 1000;
+            } else if (vainqueur === "player1") {
+                return -1000;
+            } else {
+                return 0;
+            }
+        }
+
+        // Si on n'a pas de gagnant
+        var [player1, player2] = this.nbSeries(unePartie, 2);
+
+        return player2 - player1;
+    },
+
+    /**
+     * Retourne le joueur gagnant (le joueur qui a 4 pions alignés),
+     * - "jeu_fini" si le plateau est rempli et qu'aucun joueur n'a gagné
+     * - "aucun" si le jeu n'est pas terminé et qu'aucun joueur n'a gagné
+     * @param unePartie
+     * @returns {string}
+     */
+    gagnant: function(unePartie) {
+        // On compte le nombre de séries de 4 pionq
+        var [player1, player2] = this.nbSeries(unePartie, 4);
+
+        if (player1 > 0) {
+            return "player1";
+        } else if (player2 > 0) {
+            return "player2";
+        } else {
+            if (game.plateauTotalementRempli(unePartie)) {
+                return "jeu_fini";
+            }
+        }
+        return "aucun";
+    },
+
+    /**
+     * Renvoie le nombre de séries de nb pions sur le plateau
+     * @param unePartie
+     * @param nb
+     * @returns {number[]}
+     */
+    nbSeries: function(unePartie, nb) {
+        let nbAlignes1, nbAlignes2, nbSeriesJoueur1, nbSeriesJoueur2;
+        nbAlignes1 = nbAlignes2 = nbSeriesJoueur1 = nbSeriesJoueur2 = 0;
+
+        let currentPosition;
+
+        // Verticalement : pour chaque ligne de chaque colonne
+        for (let col = 1; col <= game.nbColumn; col++) {
+            nbAlignes1 = 0; nbAlignes2 = 0;
+
+            for (let row = 1; row <= game.partie[col].length - 1; row++) {
+                currentPosition = unePartie[col][row];
+                if (currentPosition === "player1") {
+                    nbAlignes1++;
+                    // On remet le nbAlignes de l'autre joueur à 0 car sa série a été coupée
+                    nbAlignes2 = 0;
+                    if (nbAlignes1 === nb) {
+                        nbSeriesJoueur1++;
+                    }
+                } else if (currentPosition === "player2") {
+                    nbAlignes2++;
+                    // On remet le nbAlignes de l'autre joueur à 0 car sa série a été coupée
+                    nbAlignes1 = 0;
+
+                    if (nbAlignes2 === nb) {
+                        nbSeriesJoueur2++;
+                    }
+                }
+            }
+        }
+
+        nbAlignes1 = 0; nbAlignes2 = 0; currentPosition = "";
+
+        // Horizontalement : pour chaque colonne de chaque ligne
+        for (let row = 1; row <= game.nbRow; row++) {
+            for (let col = 1; col <= game.nbColumn; col++) {
+                currentPosition = unePartie[col][row];
+                if (typeof currentPosition === "undefined") {
+                    nbAlignes1 = 0; nbAlignes2 = 0;
+                } else if (currentPosition === "player1") {
+                    nbAlignes1++;
+                    // On remet le nbAlignes de l'autre joueur à 0 car sa série a été coupée
+                    nbAlignes2 = 0;
+                    if (nbAlignes1 === nb) {
+                        nbSeriesJoueur1++;
+                    }
+                } else if (currentPosition === "player2") {
+                    nbAlignes2++;
+                    // On remet le nbAlignes de l'autre joueur à 0 car sa série a été coupée
+                    nbAlignes1 = 0;
+
+                    if (nbAlignes2 === nb) {
+                        nbSeriesJoueur2++;
+                    }
+                }
+            }
+        }
+
+        // todo : diagonales (montantes et descendantes)
+
+        return [nbSeriesJoueur1, nbSeriesJoueur2];
+    },
+};
---
+++ logic.js
@@ -0,0 +1,219 @@
+;(function ($) {
+    var oAddClass = $.fn.addClass;
+    $.fn.addClass = function () {
+        for (var i in arguments) {
+            var arg = arguments[i];
+            if ( !! (arg && arg.constructor && arg.call && arg.apply)) {
+                setTimeout(arg.bind(this));
+                delete arguments[i];
+            }
+        }
+        return oAddClass.apply(this, arguments);
+    }
+
+})(jQuery);
+
+var game = {
+    nbColumn: 7,
+    nbRow: 6,
+    plateau: [],
+    partie: [],
+    joueurActif: 'player1',
+    nbPointP1: 0,
+    nbPointP2: 0,
+
+
+    /**
+     * Initialise la pertie
+     */
+    init: function () {
+        // Réinitialisation des paramètres de la partie
+        game.joueurActif = 'player1';
+        game.plateau = [];
+        game.partie = [];
+
+        // Réinitialisation du nombre de points
+        game.setNbPoints(0,0);
+
+        // Construction du plateau
+        game.construirePlateau();
+
+        // Enregistrement des évènements
+        game.bindColumn();
+    },
+
+    /**
+     * Gère l'affichage sur le hover et ajoute une pièce au clic
+     */
+    bindColumn: function() {
+        $('.column').click(function () {
+            game.placerPiece($(this).attr('id'), game.joueurActif, true);
+        }).hover(
+            function() {
+                var row = game.getLastFreeRowPosition($(this).attr('id'));
+                if (row > 0 && game.joueurActif === 'player1') $("#" + $(this).attr('id') + "_" + row).addClass(game.joueurActif + 'Hover');
+            },
+            function() {
+                var row = game.getLastFreeRowPosition($(this).attr('id'));
+                if (row > 0 && game.joueurActif === 'player1') $("#" + $(this).attr('id') + "_" + row).removeClass(game.joueurActif + 'Hover');
+            });
+    },
+
+    /**
+     * Set le nombre de points pour chaque joueur
+     * @param nbPointP1
+     * @param nbPointP2
+     */
+    setNbPoints: function(nbPointP1, nbPointP2) {
+        game.nbPointP1 = nbPointP1 === undefined ? game.nbPointP1 : nbPointP1;
+        game.nbPointP2 = nbPointP2 === undefined ? game.nbPointP2 : nbPointP2;
+        $('#points').html('<p>' +
+            '<span class="player1">Joueur 1 (' + game.nbPointP1 + ')</span> - ' +
+            '<span class="player2">Joueur 2 (' + game.nbPointP2 + ')</span>' +
+        '</p>');
+    },
+
+    /**
+     * Construit le plateau avec le bon nombre de colonnes et de lignes
+     */
+    construirePlateau: function () {
+        var elemPlateau = $('#plateau');
+        elemPlateau.empty();
+        for (var col = 1; col <= game.nbColumn; col++) {
+            game.plateau[col] = [];
+            game.partie[col] = [];
+            var elemeCol = $('<div class="column" id="' + col + '"></div>');
+            for (var row = game.nbRow; row >= 1; row--) {
+                var elem = $('<div class="row" id="' + col + '_' + row + '"></div>');
+                elemeCol.append(elem);
+                game.plateau[col][row] = elem;
+            }
+            elemPlateau.append(elemeCol);
+        }
+    },
+
+    /**
+     * Met à jour l'affichage du plateau à partir de la partie
+     * @param unePartie
+     */
+    majPlateau: function(unePartie) {
+        for (let col = 1; col <= game.nbColumn; col++) {
+            for (let row = 1; row <= game.nbRow; row++) {
+                let currentPosition = unePartie[col][row];
+                if (typeof currentPosition !== "undefined") {
+                    $("#" + col + "_" + row).addClass(currentPosition);
+                    $(".row").removeClass("player1Hover player2Hover");
+                }
+            }
+        }
+    },
+
+    /**
+     * Place une pièce dans la colonne 'col' pour le joueur 'joueur'
+     * Si 'render' est vrai, alors on met à jour l'affichage et on vérifie s'il existe un gagant
+     * @param col
+     * @param joueur
+     * @param render
+     */
+    placerPiece: function (col, joueur, render = false) {
+        var row = this.getLastFreeRowPosition(col);
+        if (row > 0) {
+            //On remplit le tableau de la partie
+            this.partie[col][row] = joueur;
+            if (render) {
+                this.majPlateau(game.partie);
+                // Vérification du cas de victoire
+                var gagnant = IA.gagnant(this.partie);
+                if(gagnant === "player1" || gagnant === "player2")
+                    game.victoire();
+                else
+                    game.switchActif();
+            }
+        }
+        return row;
+    },
+
+    /**
+     * Retire la pièce de coordonnées 'col' et 'row'
+     * @param col
+     * @param row
+     */
+    retirerPiece: function (col, row) {
+        //var row = game.getLastFreeRowPosition(col);
+        if (row > 0) {
+            //On supprimer le dernier élément de tableau de la partie
+            game.partie[col].splice(row, 1);
+        }
+    },
+
+    /**
+     * Renvoie le numéro de la dernière position libre d'une colonne
+     * @param col
+     * @returns {*}
+     */
+    getLastFreeRowPosition: function(col) {
+        var length = this.partie[col].length;
+        if (length >= game.nbRow + 1) return false;
+        return length === 0 ? 1 : length;
+    },
+
+    /**
+     * Change le joueur actif
+     * Si c'est au tour de l'IA, alors on la fait jouer
+     */
+    switchActif: function() {
+        if (game.joueurActif === 'player1') {
+            game.joueurActif = 'player2';
+            IA.jouer(this.partie, 5);
+        } else {
+            game.joueurActif = 'player1';
+        }
+    },
+
+    /**
+     * Lance la victoire du joueur courant
+     */
+    victoire: function () {
+        // todo : supprimer
+        alert('victoire');
+        $('.column').unbind();
+        if (game.joueurActif === 'player1')
+            game.nbPointP1++;
+        else
+            game.nbPointP2++;
+        game.setNbPoints();
+        // todo : décommenter
+        //game.confirmNouvelleManche();
+    },
+
+    /**
+     * Pour lancer une nouvelle manche
+     */
+    confirmNouvelleManche: function () {
+        if (confirm('Victoire ' + game.joueurActif + '\nCommencer une nouvelle manche ?')) {
+            $('#plateau').unbind('click').find('.row.player1,.row.player2').removeClass('player1 player2');
+            game.bindColumn();
+            game.switchActif();
+        } else {
+            $('#plateau').unbind('click').click(game.confirmNouvelleManche);
+        }
+    },
+
+    /**
+     * Renvoie true si le plateau est totalement rempli
+     * @param unePartie
+     * @returns {boolean}
+     */
+    plateauTotalementRempli: function(unePartie) {
+        var nbColRemplies = 0;
+        for (var col=1; col <= game.nbColumn; col++) {
+            // Si la dernière ligne est remplit alors la colonne est remplit
+            nbColRemplies += (unePartie[col].length - 1 === game.nbRow ? 1 : 0);
+        }
+        return nbColRemplies === game.nbColumn;
+    },
+};
+
+$(document).ready(function () {
+    game.init();
+});
